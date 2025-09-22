b015f437075211f72a86924cd8bee4337933b6f1
Nameless <pintokevin2002@hotmail.com>
2025-09-22 17:10:00 +0200

    Add email validation method in HelpMethods class
    This commit introduces a new method `IsValidEmail` in the `HelpMethods` class to validate email addresses. The method utilizes the `MailAddress` class to check if the provided email is valid, returning `true` for valid emails and `false` otherwise. Additionally, the `System.Net.Mail` namespace has been included to support the use of `MailAddress`.

--- Utils/HelpMethods.cs
+++ Utils/HelpMethods.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
 using TuneMates_Backend.DataBase;
 
 namespace TuneMates_Backend.Utils
@@ -8,6 +9,19 @@ namespace TuneMates_Backend.Utils
         public static async Task<bool> IsEmailInUse(AppDbContext db, string email)
         {
             return await db.Users.AnyAsync(u => u.Email == email);
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
