cc71b6db9e7137f8e6bfc053a1c659ae8d2e8244
Nameless <pintokevin2002@hotmail.com>
2025-09-22 17:41:06 +0200

    Add RoomRoutes namespace and extension method
    This commit introduces a new namespace `TuneMates_Backend.Route`
    and a static class `RoomRoutes` that includes an extension method
    `MapRoomRoutes`. This method facilitates the creation of a route
    group for operations related to "rooms" using a `RouteGroupBuilder`,
    mapping the route group to the path "/room".

---
+++ Route/RoomRoutes.cs
@@ -0,0 +1,12 @@
+﻿namespace TuneMates_Backend.Route
+{
+    public static class RoomRoutes
+    {
+        public static RouteGroupBuilder MapRoomRoutes(this RouteGroupBuilder group)
+        {
+            var roomGroup = group.MapGroup("/room");
+
+            return roomGroup;
+        }
+    }
+}
