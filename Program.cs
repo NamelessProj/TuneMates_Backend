21b08891c5603e78c522414ebc36dbbea624e7e7
Nameless <pintokevin2002@hotmail.com>
2025-09-22 17:03:20 +0200

    Remove user management routes and related functions
    This commit removes two API routes and their associated asynchronous functions, TestFunction and Test2Function, from Program.cs. TestFunction was responsible for creating a new user in the database, while Test2Function verified if a provided password matched a stored password. These changes suggest a redesign or simplification of the user management logic in the application.

--- Program.cs
+++ Program.cs
@@ -14,40 +14,5 @@ var api = app.MapGroup("/api");
 
 api.MapUserRoutes();
 
-api.MapGet("/", TestFunction);
-api.MapGet("/connect", Test2Function);
-
-async Task<IResult> TestFunction(AppDbContext db)
-{
-    string password = "password123";
-
-    var user = new User
-    {
-        Username = "testuser",
-        Email = "test3@mail.com",
-        PasswordHash = Argon2.Hash(password),
-    };
-    db.Users.Add(user);
-    await db.SaveChangesAsync();
-    return TypedResults.Ok(user);
-}
-
-async Task<IResult> Test2Function()
-{
-    string user_password_from_db = "$argon2id$v=19$m=65536,t=3,p=1$YvAWdUoDV8YPXCGbRwNdsQ$ra8jldcMAJIbxTPTfG+PmWTUqAr43/POcTil/32gPMc";
-    string password_attempt = "password123";
-
-    bool verified = Argon2.Verify(user_password_from_db, password_attempt);
-
-    if (verified)
-    {
-        return TypedResults.Ok("Password is correct!");
-    }
-    else
-    {
-        return TypedResults.Unauthorized();
-    }
-}
-
 // Launch the application.
 app.Run();
