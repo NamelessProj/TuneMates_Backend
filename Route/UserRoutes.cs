09c31c24ecb2fe9e7a0bdedc648abf24a8a12474
Nameless <pintokevin2002@hotmail.com>
2025-09-22 16:42:30 +0200

    Update user editing method in UserRoutes.cs
    This change modifies the `MapPut` method to call `UserController.EditUserById` instead of `UserController.EditUser`. This indicates that updating a user now requires a specific identifier to identify the user to be modified.

--- Route/UserRoutes.cs
+++ Route/UserRoutes.cs
@@ -11,7 +11,7 @@ namespace TuneMates_Backend.Route
             userGroup.MapGet("/", UserController.GetAllUser);
             userGroup.MapGet("/{id:int}", UserController.GetUserById);
             userGroup.MapPost("/", UserController.CreateUser);
-            userGroup.MapPut("/", UserController.EditUser);
+            userGroup.MapPut("/", UserController.EditUserById);
             userGroup.MapDelete("/", UserController.DeleteUser);
 
             return userGroup;
