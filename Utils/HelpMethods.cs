e6cc95db11f449ae47aad7c4afde20f989dcd63c
Nameless <pintokevin2002@hotmail.com>
2025-09-22 16:41:59 +0200

    Add HelpMethods class with IsEmailInUse method
    This commit introduces a new static class `HelpMethods` in the `HelpMethods.cs` file.
    It includes an asynchronous method `IsEmailInUse` that checks if an email address is already in use
    by querying the users table in the database using Entity Framework Core.

---
+++ Utils/HelpMethods.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TuneMates_Backend.DataBase;
+
+namespace TuneMates_Backend.Utils
+{
+    public static class HelpMethods
+    {
+        public static async Task<bool> IsEmailInUse(AppDbContext db, string email)
+        {
+            return await db.Users.AnyAsync(u => u.Email == email);
+        }
+    }
+}
