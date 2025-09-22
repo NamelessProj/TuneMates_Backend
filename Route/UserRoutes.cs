f488d5591591aae7b59255e1d7e0f582e7929f80
Nameless <pintokevin2002@hotmail.com>
2025-09-22 14:53:36 +0200

    Add UserRoutes class for user operations
    This commit introduces a new `UserRoutes` class in the `UserRoutes.cs` file, which defines a route group for user operations. The class includes an extension method `MapUserRoutes` to set up HTTP POST, PUT, and DELETE routes for creating, editing, and deleting users, respectively. It also utilizes the `TuneMates_Backend.Route` namespace and imports the `UserController` to handle the associated requests.

---
+++ Route/UserRoutes.cs
@@ -0,0 +1,18 @@
+﻿using TuneMates_Backend.Controller;
+
+namespace TuneMates_Backend.Route
+{
+    public static class UserRoutes
+    {
+        public static RouteGroupBuilder MapUserRoutes(this RouteGroupBuilder group)
+        {
+            var userGroup = group.MapGroup("/user");
+
+            userGroup.MapPost("/", UserController.CreateUser);
+            userGroup.MapPut("/", UserController.EditUser);
+            userGroup.MapDelete("/", UserController.DeleteUser);
+
+            return userGroup;
+        }
+    }
+}
