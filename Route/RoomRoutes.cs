4fb17727b35e73713ef1b7e11ea81807e1c80a1a
Nameless <pintokevin2002@hotmail.com>
2025-09-23 17:05:07 +0200

    Add room management routes in RoomRoutes.cs
    This update introduces a `using` directive for the `TuneMates_Backend.Controller` and implements several new routes for room operations. These routes allow retrieving a room by its ID or slug, fetching all rooms for a user, creating a room, editing a room and its password, and deleting a room. These enhancements improve the API's functionality by facilitating room management.

--- Route/RoomRoutes.cs
+++ Route/RoomRoutes.cs
@@ -1,10 +1,20 @@
-﻿namespace TuneMates_Backend.Route
+﻿using TuneMates_Backend.Controller;
+
+namespace TuneMates_Backend.Route
 {
     public static class RoomRoutes
     {
         public static RouteGroupBuilder MapRoomRoutes(this RouteGroupBuilder group)
         {
             var roomGroup = group.MapGroup("/room");
+
+            roomGroup.MapGet("/{id:int}", RoomController.GetRoomById);
+            roomGroup.MapGet("/slug/{slug:string}", RoomController.GetRoomBySlug);
+            roomGroup.MapGet("/user/{id:int}", RoomController.GetAllRoomsFromUser);
+            roomGroup.MapPost("/", RoomController.CreateRoom);
+            roomGroup.MapPut("/{id:int}", RoomController.EditRoom);
+            roomGroup.MapPut("/{id:int}/password", RoomController.EditRoomPassword);
+            roomGroup.MapDelete("/{id:int}", RoomController.DeleteRoom);
 
             return roomGroup;
         }
