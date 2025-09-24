8b892d0f2949b41ef41d92fc3bb03948037507d9
Nameless <pintokevin2002@hotmail.com>
2025-09-24 22:04:01 +0200

    Add JWT authentication and PostgreSQL configuration
    This commit introduces several changes to `Program.cs`, including the addition of `using` directives for JWT authentication and Entity Framework Core. It extracts JWT key, issuer, and audience from the configuration and adds a check to ensure these values are not null or empty, throwing an exception if they are. The authentication service is configured to use JWT with specific validation parameters. Finally, the database service is set up to use PostgreSQL with a specified connection string.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,36 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using TuneMates_Backend.DataBase;
 using TuneMates_Backend.Route;
 using TuneMates_Backend.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var jwtKey = builder.Configuration.GetValue<string>("Jwt:Key");
+var jwtIssuer = builder.Configuration.GetValue<string>("Jwt:Issuer");
+var jwtAudience = builder.Configuration.GetValue<string>("Jwt:Audience");
+
+if (string.IsNullOrEmpty(jwtKey) || string.IsNullOrEmpty(jwtIssuer) || string.IsNullOrEmpty(jwtAudience))
+    throw new ArgumentNullException("JWT configuration is missing or incomplete.");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opts =>
+    {
+        opts.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddDbContext<AppDbContext>(opts => opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
