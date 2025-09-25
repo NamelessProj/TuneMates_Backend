d76ebfd8cdab90ebd98e5bdb730eee912d52c88a
Nameless <pintokevin2002@hotmail.com>
2025-09-25 18:35:18 +0200

    Add DataBaseService class with EnsureDatabaseCreated method
    This commit introduces a new class `DataBaseService` in the `DataBaseService.cs` file.
    It includes a static method `EnsureDatabaseCreated` that utilizes an `IServiceProvider`
    to create a scope, retrieve an instance of `AppDbContext`, and apply database migrations.

---
+++ DataBase/DataBaseService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TuneMates_Backend.DataBase
+{
+    public class DataBaseService
+    {
+        public static void EnsureDatabaseCreated(IServiceProvider sp)
+        {
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.Migrate();
+        }
+    }
+}
