f830f162c0942ddabb983e2b942d8318f59c07d7
Nameless <pintokevin2002@hotmail.com>
2025-09-30 16:50:50 +0200

    Add Token class in TuneMates_Backend.DataBase namespace
    This commit introduces a new namespace `TuneMates_Backend.DataBase`
    and adds a `Token` class. The class contains four properties: `Id`,
    `RefreshToken`, `ExpiresAt`, and `CreatedAt`. The `RefreshToken`
    property is initialized to an empty string, while `CreatedAt` is
    set to the current date and time in UTC.

---
+++ DataBase/Token.cs
@@ -0,0 +1,10 @@
+﻿namespace TuneMates_Backend.DataBase
+{
+    public class Token
+    {
+        public int Id { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
