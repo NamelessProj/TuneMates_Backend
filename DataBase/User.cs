12f7521881f883d17932dde6a99ddcd40be137e0
Nameless <pintokevin2002@hotmail.com>
2025-10-20 15:09:51 +0200

    Add token management properties to User class
    Added a `Token` property to store access tokens and a `TokenExpiresAt` property to track token expiration time. These changes enable token-based authentication and authorization mechanisms by providing fields for managing access tokens and their validity periods.

--- DataBase/User.cs
+++ DataBase/User.cs
@@ -7,7 +7,9 @@
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
         public string SpotifyId { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
         public string RefreshToken { get; set; } = string.Empty;
+        public DateTime TokenExpiresAt { get; set; } = DateTime.UtcNow.AddSeconds(3600);
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
