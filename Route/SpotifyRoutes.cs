6658a90bc334eae56e9aa12075bd19d7c70ef6fb
Nameless <pintokevin2002@hotmail.com>
2025-10-20 15:26:56 +0200

    Add Spotify route group and endpoints
    Introduced a new static class `SpotifyRoutes` in the namespace `TuneMates_Backend.Route` to manage Spotify-related routes. Added the `MapSpotifyRoutes` extension method to group all Spotify routes under the `/spotify` path.
    
    Implemented two endpoints:
    - A GET route `/spotify/search/{q}/{offset:int}/{market}` for searching songs, handled by `SpotifyController.SearchSongs`.
    - A POST route `/spotify/playlist/{roomId:int}/{songId:int}` for adding songs to a playlist, requiring authorization, handled by `SpotifyController.AddSongToPlaylist`.
    
    These changes improve the structure and functionality of Spotify route management in the backend.

---
+++ Route/SpotifyRoutes.cs
@@ -0,0 +1,17 @@
+﻿using TuneMates_Backend.Controller;
+
+namespace TuneMates_Backend.Route
+{
+    public static class SpotifyRoutes
+    {
+        public static RouteGroupBuilder MapSpotifyRoutes(this RouteGroupBuilder group)
+        {
+            var spotifyGroup = group.MapGroup("/spotify");
+
+            spotifyGroup.MapGet("/search/{q}/{offset:int}/{market}", SpotifyController.SearchSongs);
+            spotifyGroup.MapPost("/playlist/{roomId:int}/{songId:int}", SpotifyController.AddSongToPlaylist).RequireAuthorization();
+
+            return spotifyGroup;
+        }
+    }
+}
