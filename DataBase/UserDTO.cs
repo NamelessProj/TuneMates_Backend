0423c0ef6fd5cf86159bedaa08e49cae1e75472d
Nameless <pintokevin2002@hotmail.com>
2025-10-21 00:29:21 +0200

    Refactor UserDTO property name for clarity
    Replaced the `ExpiresIn` property with `TokenExpiresIn` in the `UserDTO` class to improve clarity and better reflect its purpose. No other changes were made to the existing properties of the class.

--- DataBase/UserDTO.cs
+++ DataBase/UserDTO.cs
@@ -8,6 +8,6 @@
         public string PasswordConfirm { get; set; } = string.Empty;
         public string Token { get; set; } = string.Empty;
         public string RefreshToken { get; set; } = string.Empty;
-        public int ExpiresIn { get; set; }
+        public int TokenExpiresIn { get; set; }
     }
 }
