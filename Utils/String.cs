47348f24c8f14930c224ba9f9fdbc8d3c23793d9
Nameless <pintokevin2002@hotmail.com>
2025-10-24 05:32:10 +0200

    Add RemoveNonAscii method to String utility class
    A new extension method `RemoveNonAscii` was added to the partial static class `String` in the `TuneMates_Backend.Utils` namespace. This method removes non-ASCII characters from a given string using a regular expression and returns the cleaned string.

---
+++ Utils/String.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TuneMates_Backend.Utils
+{
+    public static partial class String
+    {
+        /// <summary>
+        /// Remove non-ASCII characters from a string
+        /// </summary>
+        /// <param name="s">The input string</param>
+        /// <returns>A string with non-ASCII characters removed</returns>
+        public static string RemoveNonAscii(this string s) => Regex.Replace(s, "[^\x20-\x7E]", "");
+    }
+}
