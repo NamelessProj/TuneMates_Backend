7b231c564d41b349aba94ac0c80d1e3ca87dc0f6
Nameless <pintokevin2002@hotmail.com>
2025-10-28 14:31:20 +0100

    Set default value for ExpiresAt property in Token.cs
    The `ExpiresAt` property in the `Token` class was updated to include a default value. Previously, it was a simple `DateTime` property without initialization. Now, it is initialized to the current UTC time plus one hour (`DateTime.UtcNow.AddHours(1)`).
    
    This change ensures that the `ExpiresAt` property is automatically set to a default expiration time upon object creation, simplifying token management and reducing the risk of uninitialized values.

--- DataBase/Token.cs
+++ DataBase/Token.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string RefreshToken { get; set; } = string.Empty;
-        public DateTime ExpiresAt { get; set; }
+        public DateTime ExpiresAt { get; set; } = DateTime.UtcNow.AddHours(1);
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
