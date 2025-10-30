c5f7874dd87e1708f40e993e55c51023569ab187
Nameless <pintokevin2002@hotmail.com>
2025-10-30 16:50:45 +0100

    Refactor JwtTokenValidityHours constant
    Reorganized the `JwtTokenValidityHours` constant in `Constants.cs`. The constant was removed and re-added with the same value (`3`) to improve code structure or correct potential issues. No functional changes were introduced.

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -3,9 +3,9 @@
     public static class Constants
     {
         public const int MaxRoomPerUser = 10;
+        public const int JwtTokenValidityHours = 3;
         public const int DefaultBackgroundServiceIntervalHours = 3;
         public const int MaxHoursForAProposalBeforeCleanup = 5;
-        public const int JwtTokenValidityHours = 3;
         public const int MaxHoursForARoomBeforeInactive = 24;
         public const int MaxDaysForARoomBeforeCleanup = 32;
     }
