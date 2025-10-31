74e96f54b7836159bf9a0cf6064576a9ad30017d
Pinto <pintokevin2002@hotmail.com>
2025-10-31 07:34:10 +0100

    Removing the test endpoint and adding some comments

--- Program.cs
+++ Program.cs
@@ -5,12 +5,13 @@ using TuneMates_Backend.Infrastructure.Auth;
 using TuneMates_Backend.Infrastructure.Cors;
 using TuneMates_Backend.Infrastructure.RateLimiting;
 using TuneMates_Backend.Route;
-using TuneMates_Backend.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add JWT Authentication services
 builder.Services.AddAppJwtAuthentication(builder.Configuration);
 
+// Add DbContext with PostgreSQL provider
 builder.Services.AddDbContext<AppDbContext>(opts => opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add Cron Jobs as background services
@@ -18,6 +19,7 @@ builder.Services.AddHostedService<TokenCleanupService>();
 builder.Services.AddHostedService<ProposalCleanupService>();
 builder.Services.AddHostedService<RoomCleanupService>();
 
+// Add Memory Cache
 builder.Services.AddMemoryCache();
 
 // Register JwtTokenService
@@ -30,6 +32,7 @@ builder.Services.AddAppCors(builder.Configuration);
 // Configure Rate Limiting
 builder.Services.AddAppRateLimiting(builder.Configuration);
 
+// Build the application.
 var app = builder.Build();
 
 // Activate CORS middleware
@@ -41,27 +44,13 @@ app.UseAppRateLimiting();
 // Activate JWT Authentication middleware
 app.UseAppJwtAuthentication();
 
+// Map API routes
 var api = app.MapGroup("/api");
 
 api.MapUserRoutes();
 api.MapRoomRoutes();
 api.MapSongRoutes();
 api.MapSpotifyRoutes();
-
-api.MapPost("/test/{text}", TestEndpoint).RequireAuthorization();
-
-static async Task<IResult> TestEndpoint(HttpContext http, string text)
-{
-    // Getting the user ID from the JWT token
-    var userId = HelpMethods.GetUserIdFromJwtClaims(http);
-
-    return TypedResults.Ok(new
-    {
-        Message = "Test endpoint is working!",
-        Text = text,
-        UserId = userId != null ? userId : 0
-    });
-}
 
 // Launch the application.
 app.Run();
