a7d74c434493404218df8a4c18afc7ea8ab18b39
Nameless <pintokevin2002@hotmail.com>
2025-11-01 17:52:10 +0100

    Refactor constants organization in Constants.cs
    Reorganized constants into logical static subclasses for better structure and readability.
    - Moved cleanup-related constants to a new `Cleanup` static subclass.
    - Added a new `Forms` static subclass for form-related constants.
    - Retained general constants (`MaxRoomPerUser`, `JwtTokenValidityHours`) in the main `Constants` class.
    - Removed redundant cleanup constants from the main `Constants` class after relocation.

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -2,11 +2,24 @@
 {
     public static class Constants
     {
+        public static class Cleanup
+        {
+            public const int DefaultBackgroundServiceIntervalHours = 3;
+            public const int MaxHoursForAProposalBeforeCleanup = 5;
+            public const int MaxHoursForARoomBeforeInactive = 24;
+            public const int MaxDaysForARoomBeforeCleanup = 32;
+        }
+
+        public static class Forms
+        {
+            public const int MaxUsernameLength = 30;
+            public const int MinPasswordLength = 8;
+            public const int MaxPasswordLength = 20;
+            public const int MaxEmailLength = 254;
+            public const int MaxRoomNameLength = 50;
+        }
+
         public const int MaxRoomPerUser = 10;
         public const int JwtTokenValidityHours = 3;
-        public const int DefaultBackgroundServiceIntervalHours = 3;
-        public const int MaxHoursForAProposalBeforeCleanup = 5;
-        public const int MaxHoursForARoomBeforeInactive = 24;
-        public const int MaxDaysForARoomBeforeCleanup = 32;
     }
 }
