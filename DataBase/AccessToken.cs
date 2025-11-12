cea174b094e6f3fa77599c4bdffd3f1defa65355
Nameless <pintokevin2002@hotmail.com>
2025-11-12 18:11:13 +0100

    Add AccessToken class to manage authentication tokens
    This class includes three properties:
    - `Token`: a string initialized to an empty value.
    - `RefreshToken`: a string initialized to an empty value.
    - `ExpiresIn`: an integer initialized to 3600, likely representing the token expiration time in seconds.
    
    These changes aim to handle access and refresh tokens in the backend application.

---
+++ DataBase/AccessToken.cs
@@ -0,0 +1,9 @@
+﻿namespace TuneMates_Backend.DataBase
+{
+    public class AccessToken
+    {
+        public string Token { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public int ExpiresIn { get; set; } = 3600;
+    }
+}
