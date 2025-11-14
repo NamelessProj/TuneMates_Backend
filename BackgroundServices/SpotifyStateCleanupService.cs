bde1686651af6f3005d8b796812937a2da5b91eb
Pinto <pintokevin2002@hotmail.com>
2025-11-14 11:19:05 +0100

    Add XML documentation to SpotifyStateCleanupService
    Added XML comments to document the purpose of the class, its constructor, and the `ExecuteJobAsync` method. This includes details about the parameters and the functionality of the cleanup service, which periodically removes old `SpotifyState` entries from the database.

--- BackgroundServices/SpotifyStateCleanupService.cs
+++ BackgroundServices/SpotifyStateCleanupService.cs
@@ -4,14 +4,28 @@ using TuneMates_Backend.Utils;
 
 namespace TuneMates_Backend.BackgroundServices
 {
+    /// <summary>
+    /// A background service that periodically cleans up old SpotifyState entries from the database.
+    /// </summary>
     public class SpotifyStateCleanupService : RecurringBackgroundService
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpotifyStateCleanupService"/> class.
+        /// </summary>
+        /// <param name="scopeFactory">The service scope factory for creating scopes.</param>
+        /// <param name="logger">The logger instance.</param>
+        /// <param name="config">The configuration instance.</param>
         public SpotifyStateCleanupService(
             IServiceScopeFactory scopeFactory,
             ILogger<SpotifyStateCleanupService> logger,
             IConfiguration config) : base(scopeFactory, logger, TimeSpan.FromHours(config.GetValue<double>("CleanupService:SpotifyStateIntervalHours", Constants.Cleanup.DefaultBackgroundServiceIntervalHours)))
         { }
 
+        /// <summary>
+        /// Executes the cleanup job to delete old SpotifyState entries.
+        /// </summary>
+        /// <param name="stoppingToken">The cancellation token.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         protected override async Task ExecuteJobAsync(CancellationToken stoppingToken)
         {
             using var scope = _scopeFactory.CreateScope();
