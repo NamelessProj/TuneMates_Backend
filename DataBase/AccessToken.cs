453be3a379cfdaec6c9a38e230eafa67bda1fc74
Pinto <pintokevin2002@hotmail.com>
2025-11-14 10:44:07 +0100

    Add CreatedAt property to AccessToken class
    A new property `CreatedAt` of type `DateTime` was added to the `AccessToken` class. This property is initialized by default to the current date and time in UTC (`DateTime.UtcNow`).

--- DataBase/AccessToken.cs
+++ DataBase/AccessToken.cs
@@ -5,5 +5,6 @@
         public string Token { get; set; } = string.Empty;
         public string RefreshToken { get; set; } = string.Empty;
         public int ExpiresIn { get; set; } = 3600;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
