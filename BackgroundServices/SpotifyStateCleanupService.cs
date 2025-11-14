3b4c6dbe5bf998a4ff240d787c10e0835691612d
Pinto <pintokevin2002@hotmail.com>
2025-11-14 11:11:31 +0100

    Add SpotifyStateCleanupService for periodic cleanup
    Introduced a new background service `SpotifyStateCleanupService` to periodically delete outdated entries from the `SpotifyStates` table in the database. The service uses a configurable interval and removes entries older than a defined cutoff time. Includes logging of cleanup operations and integrates with `AppDbContext` for database access.

---
+++ BackgroundServices/SpotifyStateCleanupService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TuneMates_Backend.DataBase;
+using TuneMates_Backend.Utils;
+
+namespace TuneMates_Backend.BackgroundServices
+{
+    public class SpotifyStateCleanupService : RecurringBackgroundService
+    {
+        public SpotifyStateCleanupService(
+            IServiceScopeFactory scopeFactory,
+            ILogger<SpotifyStateCleanupService> logger,
+            IConfiguration config) : base(scopeFactory, logger, TimeSpan.FromHours(config.GetValue<double>("CleanupService:SpotifyStateIntervalHours", Constants.Cleanup.DefaultBackgroundServiceIntervalHours)))
+        { }
+
+        protected override async Task ExecuteJobAsync(CancellationToken stoppingToken)
+        {
+            using var scope = _scopeFactory.CreateScope();
+            AppDbContext db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            DateTime cutoff = DateTime.UtcNow.AddHours(-Constants.Cleanup.MaxHoursForSpotifyStateBeforeCleanup); // Define cutoff time for deletion all SpotifyStates older than this
+            int deletedCount = await db.SpotifyStates
+                .Where(s => s.CreatedAt < cutoff)
+                .ExecuteDeleteAsync(stoppingToken);
+
+            _logger.LogInformation("{Service}: Deleted {DeletedCount} SpotifyState entries older than {Cutoff}", GetType().Name, deletedCount, cutoff);
+        }
+    }
+}
