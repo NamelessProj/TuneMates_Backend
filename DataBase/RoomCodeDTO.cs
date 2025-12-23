b0faf532dcc195cbc8b86352d5f3dc327a766545
Nameless <pintokevin2002@hotmail.com>
2025-12-23 23:40:18 +0100

    Ajoute la classe RoomCodeDTO pour la gestion des codes
    Création du fichier RoomCodeDTO.cs avec la classe RoomCodeDTO dans l'espace de noms TuneMates_Backend.DataBase. Cette classe inclut les propriétés Password (string) et ExpiresInHours (int) pour gérer les codes de salle et leur expiration.

---
+++ DataBase/RoomCodeDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TuneMates_Backend.DataBase
+{
+    public class RoomCodeDTO
+    {
+        public string Password { get; set; } = string.Empty;
+        public int ExpiresInHours { get; set; } = 1;
+    }
+}
