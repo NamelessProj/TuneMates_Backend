6aad54352d5626baa91e4a36cc57f6c94d53deab
Nameless <pintokevin2002@hotmail.com>
2025-12-23 23:52:11 +0100

    Réorganise l'ordre des routes POST dans RoomRoutes
    La définition de la route POST "/slug/{slug}" a été déplacée après la route POST "/" pour améliorer la lisibilité et l'organisation du code. Aucun impact fonctionnel n'est à signaler.

--- Route/RoomRoutes.cs
+++ Route/RoomRoutes.cs
@@ -17,8 +17,8 @@ namespace TuneMates_Backend.Route
             roomGroup.MapGet("/", RoomController.GetAllRoomsFromUser).RequireRateLimiting(RateLimitPolicies.SearchTight).RequireAuthorization();
             roomGroup.MapGet("/{id:int}", RoomController.GetRoomById).RequireRateLimiting(RateLimitPolicies.SearchTight).RequireAuthorization();
             roomGroup.MapGet("/{code}", RoomController.GetRoomByCode).RequireRateLimiting(RateLimitPolicies.SearchTight);
-            roomGroup.MapPost("/slug/{slug}", RoomController.GetRoomBySlug).RequireRateLimiting(RateLimitPolicies.SearchTight);
             roomGroup.MapPost("/", RoomController.CreateRoom).RequireAuthorization();
+            roomGroup.MapPost("/slug/{slug}", RoomController.GetRoomBySlug).RequireRateLimiting(RateLimitPolicies.SearchTight);
             roomGroup.MapPut("/{roomId:int}", RoomController.EditRoom).RequireRateLimiting(RateLimitPolicies.Mutations).RequireAuthorization();
             roomGroup.MapPut("/password/{id:int}", RoomController.EditRoomPassword).RequireRateLimiting(RateLimitPolicies.Mutations).RequireAuthorization();
             roomGroup.MapDelete("/{id:int}", RoomController.DeleteRoom).RequireAuthorization();
