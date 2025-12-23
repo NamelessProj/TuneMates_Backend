b74db5f5936d2bd544e6d5739968e0e1faa7897b
Nameless <pintokevin2002@hotmail.com>
2025-12-24 00:33:10 +0100

    Ajout route POST pour obtenir le code d'une salle
    Ajout d'une nouvelle route POST "/code/{roomId:int}" dans RoomRoutes.cs permettant de récupérer le code d'une salle via son identifiant. La route utilise la méthode GetCodeForARoom du RoomController, applique la limitation de débit "Mutations" et requiert une autorisation.

--- Route/RoomRoutes.cs
+++ Route/RoomRoutes.cs
@@ -18,6 +18,7 @@ namespace TuneMates_Backend.Route
             roomGroup.MapGet("/{id:int}", RoomController.GetRoomById).RequireRateLimiting(RateLimitPolicies.SearchTight).RequireAuthorization();
             roomGroup.MapGet("/{code}", RoomController.GetRoomByCode).RequireRateLimiting(RateLimitPolicies.SearchTight);
             roomGroup.MapPost("/", RoomController.CreateRoom).RequireAuthorization();
+            roomGroup.MapPost("/code/{roomId:int}", RoomController.GetCodeForARoom).RequireRateLimiting(RateLimitPolicies.Mutations).RequireAuthorization();
             roomGroup.MapPost("/slug/{slug}", RoomController.GetRoomBySlug).RequireRateLimiting(RateLimitPolicies.SearchTight);
             roomGroup.MapPut("/{roomId:int}", RoomController.EditRoom).RequireRateLimiting(RateLimitPolicies.Mutations).RequireAuthorization();
             roomGroup.MapPut("/password/{id:int}", RoomController.EditRoomPassword).RequireRateLimiting(RateLimitPolicies.Mutations).RequireAuthorization();
